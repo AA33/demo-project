94a3144c4703cc654f36d6e81d054937ff01512f
Abhishek Anurag <anurag.abhishek3@gmail.com>
2014-07-23 22:20:23 -0500

    Google maps integration

--- travel_log_demo/travel_log/static/travel_log/js/preview.js
+++ travel_log_demo/travel_log/static/travel_log/js/preview.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    //Raty Integration
     var hidden_ratings = $("div[name*=hidden_rating_]");
     var rating_divs = $("div[name^=rating]");
 
@@ -9,15 +10,58 @@ $(document).ready(function () {
             score: parseInt(hidden_ratings[i].innerHTML)
         });
     }
+    //Raty integration end
+
+    //Fix map div height
+    var left_ht = $("#details").height()
+    if(left_ht>300){
+        $("map-canvas").height(left_ht);
+    }
 
 
-    function initialize(x) {
+    //Google maps integration
+    function mapInitialize() {
         var mapOptions = {
-            center: new google.maps.LatLng(x, 150.644),
-            zoom: 8
+            center: new google.maps.LatLng(0, 0),
+            zoom: 3
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
+
+
+        function placeMarker(location) {
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map
+            });
+
+            map.setCenter(location);
+        }
+
+
+        var lat_divs = $("div[name*=hidden_lat_]");
+        var lng_divs = $("div[name*=hidden_lng_]");
+        var path_coords = [];
+        var lat, lng, location;
+        for (var i = 0; i < lat_divs.length; i++) {
+            lat = parseFloat(lat_divs[i].innerHTML);
+            lng = parseFloat(lng_divs[i].innerHTML);
+            location = new google.maps.LatLng(lat, lng);
+            placeMarker(location);
+            path_coords.push(location);
+        }
+
+        var path = new google.maps.Polyline({
+            path: path_coords,
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+        path.setMap(map);
     }
-    google.maps.event.addDomListener(window, 'load', initialize(-34.397));
+
+    google.maps.event.addDomListener(window, 'load', mapInitialize());
+
+    //Google maps Integration end
 });
