4a3392d27540809c4fa4c4499b98f76b49ea6599
Abhishek Anurag <anurag.abhishek3@gmail.com>
2014-08-09 13:52:27 -0500

    Renamed constants

--- travel_log/travel_log/constants.py
+++ travel_log/travel_log/constants.py
@@ -2,9 +2,10 @@ __author__ = 'abhishekanurag'
 
 TLG_APP_NAME = 'travel_log'
 
-TLG_GET = 'GET'
-TLG_POST = 'POST'
-TLG_DELETE = 'DELETE'
+HTTP_GET = 'GET'
+HTTP_PUT = 'PUT'
+HTTP_POST = 'POST'
+HTTP_DELETE = 'DELETE'
 
 TLG_ERR_MSG = 'error_message'
 TLG_NEW = 'new'
