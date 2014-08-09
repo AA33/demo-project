e5acd1a8d09d4a6fe3a038c96711f76b0d7367fb
Abhishek Anurag <anurag.abhishek3@gmail.com>
2014-08-09 13:59:36 -0500

    Allow all hosts, for testing only.

--- travel_log/demo/settings.py
+++ travel_log/demo/settings.py
@@ -26,7 +26,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # List of callables that know how to import templates from various sources.
