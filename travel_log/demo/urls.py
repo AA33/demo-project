7be6eff02994aa9ac4b013d6cd61da919b67704c
Abhishek Anurag <anurag.abhishek3@gmail.com>
2014-08-09 14:24:34 -0500

    Added robots.txt

--- travel_log/demo/urls.py
+++ travel_log/demo/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 from django.contrib import admin
 
 admin.autodiscover()
@@ -7,4 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        url(r'^travel_log/', include('travel_log.urls', namespace='travel_log')),
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'^robots\.txt$',
+                           TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 )
